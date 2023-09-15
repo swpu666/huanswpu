36aa8bf8bd3cb5af2d025aeaf1a411d58149a037
swpu666 <105863301+swpu666@users.noreply.github.com>
2023-09-15 21:34:29 +0800

    EmployeeMapper

---
+++ src/main/java/com/itswpu/huanswpu/mapper/CategoryEmployeeMapper.java
@@ -0,0 +1,10 @@
+package com.itswpu.huanswpu.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.itswpu.huanswpu.entity.Category;
+import com.itswpu.huanswpu.entity.CategoryEmployee;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CategoryEmployeeMapper extends BaseMapper<CategoryEmployee> {
+}
---
+++ src/main/java/com/itswpu/huanswpu/mapper/SetmealEmployeeMapper.java
@@ -0,0 +1,10 @@
+package com.itswpu.huanswpu.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.itswpu.huanswpu.entity.OrdersEmployee;
+import com.itswpu.huanswpu.entity.SetmealEmployee;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SetmealEmployeeMapper extends BaseMapper<SetmealEmployee> {
+}
