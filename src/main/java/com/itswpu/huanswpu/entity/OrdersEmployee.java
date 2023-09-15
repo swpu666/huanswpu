ae55650f64432f0c36aca4e0b85c7f7a19ed55f7
fengcheche01 <2286737897@qq.com>
2023-09-15 19:44:30 +0800

    更新实体类

---
+++ src/main/java/com/itswpu/huanswpu/entity/OrdersEmployee.java
@@ -0,0 +1,40 @@
+package com.itswpu.huanswpu.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class OrdersEmployee {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private int isDeleted;
+    private Long orderId;
+
+    private Long employeeId;
+    //分类名称
+    private String name;
+
+    //创建时间
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+
+
+    //更新时间
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+
+
+    //创建人
+    @TableField(fill = FieldFill.INSERT)
+    private Long createUser;
+
+
+    //修改人
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Long updateUser;
+}
