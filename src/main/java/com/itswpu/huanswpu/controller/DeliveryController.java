47ae3d826a5ee7d2c7c09344a7d69d6612250159
swpu666 <105863301+swpu666@users.noreply.github.com>
2023-09-15 21:30:15 +0800

    Delivery 细节

--- src/main/java/com/itswpu/huanswpu/controller/DeliveryController.java
+++ src/main/java/com/itswpu/huanswpu/controller/DeliveryController.java
@@ -2,8 +2,9 @@ package com.itswpu.huanswpu.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.itswpu.huanswpu.common.R;
+import com.itswpu.huanswpu.entity.Delivery;
 import com.itswpu.huanswpu.entity.User;
-import com.itswpu.huanswpu.service.UserService;
+import com.itswpu.huanswpu.service.DeliveryService;
 import com.itswpu.huanswpu.utils.ValidateCodeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
@@ -24,7 +25,7 @@ import java.util.concurrent.TimeUnit;
 public class DeliveryController {
 
     @Autowired
-    private UserService userService;
+    private DeliveryService deliveryService;
     @Autowired
     private RedisTemplate redisTemplate;
 
@@ -63,7 +64,7 @@ public class DeliveryController {
      * @return
      */
     @PostMapping("/login")
-    public R<User> login(@RequestBody Map map, HttpSession session){
+    public R<Delivery> login(@RequestBody Map map, HttpSession session){
         log.info(map.toString());
         //获取手机号
         String phone = map.get("phone").toString();
@@ -78,21 +79,21 @@ public class DeliveryController {
         //进行验证码的比对（页面提交的验证码和Session中保存的验证码比对）
         if(codeInSession != null && codeInSession.equals(code)){
             //如果能够比对成功，说明登录成功
-            LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
-            queryWrapper.eq(User::getPhone,phone);
+            LambdaQueryWrapper<Delivery> queryWrapper = new LambdaQueryWrapper<>();
+            queryWrapper.eq(Delivery::getPhone,phone);
 
-            User user = userService.getOne(queryWrapper);
-            if(user == null){
+            Delivery delivery = deliveryService.getOne(queryWrapper);
+            if(delivery == null){
                 //判断当前手机号对应的用户是否为新用户，如果是新用户就自动完成注册
-                user = new User();
-                user.setPhone(phone);
-                user.setStatus(1);
-                userService.save(user);
+                delivery = new Delivery();
+                delivery.setPhone(phone);
+                delivery.setStatus(1);
+                deliveryService.save(delivery);
             }
-            session.setAttribute("user",user.getId());
+            session.setAttribute("currentThread",delivery.getId());
             //如果用户登录成功，删除redis中缓存的验证码
             redisTemplate.delete(phone);
-            return R.success(user);
+            return R.success(delivery);
         }
         return R.error("登录失败");
     }
